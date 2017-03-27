f6ab3fa45f9c3c08bb8b0c112b43e11adb403e6a
b4015474 <b4015474@my.shu.ac.uk>
2017-03-27 13:00:53 +0100

    Domain classses, relationships, controllers and working bootstrap setup, pre database setup

--- librarySystem/grails-app/init/librarysystem/BootStrap.groovy
+++ librarySystem/grails-app/init/librarysystem/BootStrap.groovy
@@ -3,79 +3,56 @@ package com.mkenyon
 class BootStrap {
 
     def init = { servletContext ->
-    
+
 def library1= new Library (
+	building:'library1',
+	address:'25 street',
+	openingHours:'9-7',
+	location:'city',
+	studySpaces:'200').save()
 
-      building:'Library',
-      address:'Arundel',
-      openingHours:'9-12',
-      location:'City',
-      studySpaces:'200'
+def course1= new Course  (
+	title:'Computing',
+	code:'2510',  
+	leader:'Chloe Harvey',
+	department:'Computing',
+	description:'This is a computing course',
+	studyMode:'Full-time').save()
 
-).save()
+def stud1 = new Student (
+	name:'Amy Moody',
+	email:'a.moody@gmail.com',
+	username:'amoody',
+	password:'password',
+	studentID:'b5001264',
+	course:course1,
+	library: library1 ).save()
 
+def book1 = new Book (
+	title:'Computing Beginners',
+	subject:'Computing', 
+	author:'MKenyon',
+	isbn:'25101996',
+	dateBorrowed: new Date('12/05/2015'),
+	returnDate: new Date('17/05/2017'),
+	student: stud1,
+	overdue: false,
+	library: library1).save()
 
-def course1= new Course (
+def bookReview1 = new BookReview ( 
+	book: book1,
+	dateMade: new Date('12/06/2010'),
+	student: stud1,
+	review:'This book was very helpful').save()
 
-      title:'Computing',
-      code:'123',
-      leader:'Tom Jones',
-      department:'Computers',
-      description:'Test Course',
-      studyMode:'Full-time'
-
-).save()
-
-
-def student1= new Student (
-
-      name:'Miles Kenyon',
-      email:'miles@shu.ac.uk',
-      username:'mk96',
-      password:'password',
-      studentID:'b4015474',
-      course:course1,
-      library:library1
-
-).save()
-
-
-def book1= new Book (
-
-      title:'Computing for dummies',
-      subject:'learning',
-      author:'Miles',
-      isbn:'12345',
-      dateBorrowed: new Date('01/01/2017'),
-      returnDate: new Date('02/02/2017'),
-      student: student1,
-      overdue: true,
-      library: library1
-
-).save()
-
-
-def bookreview1= new BookReview (
-
-      book: book1,
-      dateMade: new Date('03/03/2017'),
-      student: student1,
-      review: 'Good'
-
-).save()
-
-
-def librarian1= new Librarian (
-
-      name:'David',
-      email:'david@lib.uk',
-      office:'office',
-      username:'david17',
-      password:'password',
-      telephone:'078742585402',
-      library:library1
-
-).save()      
+def librarian1 = new Librarian (
+	name:'Jack Rodgers', 
+	email:'j.rodgers@librarian.com',
+	office:'cantor',
+	username:'j.rodgers',
+	password:'password',
+	telephone:'0114 569 578',
+	library: library1).save()
 
 }
     def destroy = {
